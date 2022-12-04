584abb55a92c35bd47784431d496756bd4ce52d0
Berdem7 <byambaerdene.gansukh@gmail.com>
2022-12-04 20:52:17 +0800

    [ADD]

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Invescore Happy Hour
+        {/* Invescore Happy Hour */}
         <a href="https://www.vectorstock.com/royalty-free-vector/lightning-bolt-set-neon-signs-design-template-vector-43755560"></a>
         <p>
           <a href="https://giphy.com/gifs/neon-dark-sphere-MBfLHGvyyeQgH8UknX"></a>
@@ -16,7 +16,7 @@ function App() {
           <img className="max-w-full" src={logo} alt="" />
         </div>
         <div className="h-96">
-          <p className="m-auto my-5">You are invited to the</p>
+          {/* <p className="m-auto my-5">You are invited to the</p> */}
           <p id="party" className="text-2xl">
             Neon Party!!!
           </p>
